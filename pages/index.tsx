d54e4f5d069f682037e7f74307551cf175d6a08e
takuma <>
2021-01-03 15:07:15 +0900

    Feat: Link試し設置

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
-export default () => <h1>Next.jsアプリのトップページ with TypeScript</h1>
+import Link from 'next/link'
+export default () => {
+  return (
+    <>
+      <h1>Next.jsアプリのトップページ with TypeScript</h1>
+      <Link href="/posts/first-post"><a>read first post</a></Link>
+    </>
+  )
+}

d859cf4d4ccab588d1842f9eb9cd2616aaa30ca6
takuma <>
2021-01-10 15:53:01 +0900

    style: CSS Module追加

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
       <Link href="/"><a>go to index</a></Link>
-    </>
+    </Layout>
   )
 }

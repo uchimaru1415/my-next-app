87011672838ff6a71d0ba4d98bdbf66be95ba636
takuma <>
2021-01-10 16:15:00 +0900

    Feat: プロフィール画像設定、全体のCSS調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
-import Link from 'next/link'
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
-export default () => {
+export default function Home() {
   return (
-    <>
-      <h1>Next.jsアプリのトップページ with TypeScript</h1>
-      <Link href="/posts/first-post"><a>read first post</a></Link>
-      <footer>
-        <img src="/img/next_logo.png" alt="next_logo" className="logo" />
-      </footer>
-    </>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   )
 }

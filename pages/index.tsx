e53decdf4f7254169a673f2fc360fa340e374f6d
takuma <>
2021-01-10 15:24:39 +0900

    Feat: 画像参照テスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ export default () => {
     <>
       <h1>Next.jsアプリのトップページ with TypeScript</h1>
       <Link href="/posts/first-post"><a>read first post</a></Link>
+      <footer>
+        <img src="/img/next_logo.png" alt="next_logo" className="logo" />
+      </footer>
     </>
   )
 }

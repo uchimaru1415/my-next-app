2db28361d129a95af2739adf636647e9f9efd243
takuma <>
2021-01-10 15:32:35 +0900

    Feat: headタグの情報を追加

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
-    <h1>First Post</h1>
-    <Link href="/"><a>go to index</a></Link>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Link href="/"><a>go to index</a></Link>
     </>
   )
 }
